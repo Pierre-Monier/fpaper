add9075c055744460225411af77e1d0ac51b9ac3
Pierre-Monier <p.monier96@gmail.com>
2022-07-30 16:47:57 +0200

    feat(auth): create example app

--- packages/auth/example/lib/main.dart
+++ packages/auth/example/lib/main.dart
@@ -1,26 +1,13 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:fpaper/firebase_options.dart';
-import 'package:wallpaper/wallpaper.dart';
 
-Future<void> _messageHandler(RemoteMessage message) {
-  final url = message.data["url"] as String;
-  return setAndroidWallpaper(url);
-}
-
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  FirebaseMessaging.onBackgroundMessage(_messageHandler);
+void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp();
+  const MyApp({Key? key}) : super(key: key);
 
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -34,7 +21,16 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({required this.title});
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  
+  
+  
+
+  
+  
+  
+  
 
   final String title;
 
@@ -43,41 +39,68 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  late FirebaseMessaging messaging;
-  @override
-  void initState() {
-    super.initState();
-    messaging = FirebaseMessaging.instance;
-    messaging.getToken().then((value) {
-      print(value);
-    });
+  int _counter = 0;
 
-    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-      final url = message.data["url"] as String;
-      setAndroidWallpaper(url);
+  void _incrementCounter() {
+    setState(() {
+      
+      
+      
+      
+      
+      _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    
+    
+    
+    
+    
+    
     return Scaffold(
       appBar: AppBar(
+        
+        
         title: Text(widget.title),
       ),
       body: Center(
+        
+        
         child: Column(
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
               'You have pushed the button this many times:',
             ),
             Text(
-              "totoo",
+              '$_counter',
               style: Theme.of(context).textTheme.headline4,
             ),
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), 
     );
   }
 }
